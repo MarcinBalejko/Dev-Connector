b822b5d1aec6fc38eb7f9a55d4c1f0d103e60950
MarcinBalejko <mbbm9@o2.pl>
2020-03-14 00:42:11 +0100

    :white_check_mark: Implemented bcryptjs

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
@@ -53,6 +54,11 @@ router.post(
       });
 
       // Encrypt password (bcrypt)
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt); // creating and putting hash into user.password
+
+      await user.save();
 
       // Return jsonwebtoken
       res.send("User route");
