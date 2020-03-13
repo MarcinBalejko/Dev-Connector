4ab2e991610f6c4bff59ea3a45f679900e1b182a
MarcinBalejko <mbbm9@o2.pl>
2020-03-14 00:17:05 +0100

    :recycle: Checking for a user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
 const { check, validationResult } = require("express-validator");
+
+const User = require("../../models/User");
 
 // @route   POSTapi/users
 // @desc    Register user
@@ -17,13 +20,33 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       // if there are errors
       return res.status(400).json({ errors: errors.array() }); // 400 = bad request
     }
-    res.send("User route");
+
+    const { name, email, password } = req.body;
+
+    try {
+      // See if user exists
+      let user = await User.findOne({ email }); //since they're the same, we can just type single email instead of email: email
+
+      if (user) {
+        res.status(400).json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      // Get users gravatar (based on their email)
+
+      // Encrypt password (bcrypt)
+
+      // Return jsonwebtoken
+      res.send("User route");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
