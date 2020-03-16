277ab405e14021eff71575b093e05ff1f10337e7
MarcinBalejko <mbbm9@o2.pl>
2020-03-16 02:04:46 +0100

    :white_check_mark::zap: Removing profile and & user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -129,7 +129,7 @@ router.get("/", async (req, res) => {
 });
 
 // @route   GET api/profile/user/:user_id
-// @desc    Get all profile by user ID
+// @desc    Get profile by user ID
 // @access  Public
 router.get("/user/:user_id", async (req, res) => {
   try {
@@ -144,6 +144,23 @@ router.get("/user/:user_id", async (req, res) => {
     if (err.kind == "ObjectId") {
       return res.status(400).json({ msg: "Profile not found" });
     }
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   DELETE api/profile
+// @desc    Delete profile, user & posts
+// @access  Private
+router.delete("/", auth, async (req, res) => {
+  try {
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: "User deleted" });
+  } catch (error) {
+    console.error(error.message);
     res.status(500).send("Server Error");
   }
 });
