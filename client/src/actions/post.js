d155bba84f7656a6e6af752027589351a73e8428
MarcinBalejko <mbbm9@o2.pl>
2020-04-01 16:48:47 +0200

    :white_check_mark: Add and remove like actions

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -18,3 +18,37 @@ export const getPosts = () => async dispatch => {
     });
   }
 };
+
+// Add like
+export const getLike = id => async dispatch => {
+  try {
+    const res = await axios.get(`/api/posts/like/${postId}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: res.data }
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Remove like
+export const removeLike = id => async dispatch => {
+  try {
+    const res = await axios.get(`/api/posts/unlike/${postId}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: res.data }
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
