996b1a50aa4bc2c494bbcd0b70bdaf3fb7cf679e
MarcinBalejko <mbbm9@o2.pl>
2020-04-02 00:50:03 +0200

    :recycle::white_check_mark: App debugged, post form completed

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -79,14 +79,14 @@ export const deletePost = id => async dispatch => {
 };
 
 // Add post
-export const addPost = id => async dispatch => {
+export const addPost = formData => async dispatch => {
   const config = {
     headers: {
       "Content-Type": "application/json"
     }
   };
   try {
-    const res = await axios.post(`/api/posts`, formData, config);
+    const res = await axios.post("/api/posts", formData, config);
 
     dispatch({
       type: ADD_POST,
